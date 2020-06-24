637a542aa2b4d6089b0a721d223c3a7d0688952a
uzeal <uzeal07@gmail.com>
2020-06-24 17:05:41 -0500

    fix dir handling

--- make-sea-of-thieves.sh
+++ make-sea-of-thieves.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-pushd patches
-sh patch-sot.sh &> sotpatch.log
+
+#WINE
+pushd wine
+git reset --hard HEAD
+git clean -xdf
 popd
+
+#Sea of Thieves
+pushd patches
+sh patch-sot.sh &> ../sotpatch.log
+popd
+
+#make proton-sot
 build_name=proton-sot make redist | tee build-sot.log
